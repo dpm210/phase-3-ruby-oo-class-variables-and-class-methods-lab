615bd1807ae64efecb4dd817a6d88e904f651b8c
Daniel Mueller <danielpetermueller@gmail.com>
2021-06-29 21:15:06 -0400

    commit tree ff7727718edea645c3ee0ac430afeebb6956094e

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+require "pry"
+class Song
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    attr_accessor :name, :artist, :genre
+    def initialize (name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    # def artist.count
+    # end
+
+    def genre.count
+        genre_count = {}
+        @@genres.each do |genre|
+            genre_count += 1 
+    end
+
+
+
+    lucifer = Song.new("Lucifer", "Jay-Z", "Rap")
+
+end
